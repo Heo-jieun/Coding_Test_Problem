869666b2ff911016db791afe6f6943f30cb44632
HEO JIEUN <65994153+Heo-jieun@users.noreply.github.com>
2023-10-12 23:05:44 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    jjack = 0
+    hol = 0
+    for num in num_list : 
+        if num % 2 == 0:
+            jjack += 1
+        else : 
+            hol += 1
+    answer = [jjack, hol]
+    return answer
